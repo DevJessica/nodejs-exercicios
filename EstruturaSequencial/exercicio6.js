a94ff2da1f5fd97775392f3015ab9e3ba93703eb
Jessica Cristina <37486644+DevJessica@users.noreply.github.com>
2019-06-12 23:42:55 -0300

    feito os exercícios 6 e 7

---
+++ EstruturaSequencial/exercicio6.js
@@ -0,0 +1,6 @@
+//6 Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+
+let raio = Math.floor(Math.random() * 10);
+let area = 3.14 * (raio * raio);
+
+console.log(" O resultado da area e " ,area);
---
+++ EstruturaSequencial/exercicio7.js
@@ -0,0 +1,7 @@
+//7 Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
+
+let lado = Math.floor(Math.random() * 10);
+let quadrado = lado * lado;
+let dobro = quadrado * 2;
+
+console.log(" A area do quadrado e " + quadrado + ", e o seu dobro e " + dobro);
